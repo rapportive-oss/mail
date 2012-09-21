0ba2f63316af6471ca109635790c29cd3bf95d14
Conrad Irwin <conrad.irwin@gmail.com>
2012-09-20 17:11:34 -0700

    "Fix" UndefinedConversionErrors

--- lib/mail/fields/unstructured_field.rb
+++ lib/mail/fields/unstructured_field.rb
@@ -139,7 +139,13 @@ module Mail
         line = ""
         while !words.empty?
           break unless word = words.first.dup
-          word.encode!(charset) if defined?(Encoding) && charset
+          if defined?(Encoding) && charset
+            begin
+              word.encode!(charset)
+            rescue Encoding::UndefinedConversionError
+              should_encode = true
+            end
+          end
           word = encode(word) if should_encode
           word = encode_crlf(word)
           # Skip to next line if we're going to go past the limit
