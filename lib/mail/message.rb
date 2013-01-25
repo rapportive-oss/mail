afd7c6a1d91de9c5854f14317379d4c78a970b7e
Conrad Irwin <conrad.irwin@gmail.com>
2013-01-25 12:51:04 -0800

    [HACK] Import rapportive encoding hacks
    Conflicts:
    	lib/mail/message.rb

--- lib/mail/message.rb
+++ lib/mail/message.rb
@@ -1780,6 +1780,7 @@ module Mail
     # ready to send
     def ready_to_send!
       header.ready_to_send!
+      body.charset = charset
       identify_and_set_transfer_encoding
       parts.sort!([ "text/plain", "text/enriched", "text/html", "multipart/alternative" ]) if body.need_to_sort?
       parts.each do |part|
@@ -2019,6 +2020,9 @@ module Mail
     end
 
     def add_encoding_to_body
+      if has_charset?
+        @body.charset = charset
+      end
       if has_content_transfer_encoding?
         @body.encoding = content_transfer_encoding
       end
