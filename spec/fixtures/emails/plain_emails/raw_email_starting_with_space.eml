rapportive-oss/mail

commit 17783f8536fc09b926c7425dbacfc35e0e851ef5

Add mail parse fail example and fix to pass the spec.
Conflicts:

	spec/mail/example_emails_spec.rb

lib/mail/message.rb
@@ -1875,10 +1875,7 @@ module Mail
     # Additionally, I allow for the case where someone might have put whitespace
     # on the "gap line"
     def parse_message
-      header_part, body_part = raw_source.split(/#{CRLF}#{WSP}*#{CRLF}/m, 2)
-#      index = raw_source.index(/#{CRLF}#{WSP}*#{CRLF}/m, 2)
-#      self.header = (index) ? header_part[0,index] : nil
-#      lazy_body ( [raw_source, index+1])
+      header_part, body_part = raw_source.split(/#{CRLF}#{WSP}*#{CRLF}(?!#{WSP})/m, 2)
       self.header = header_part
       self.body   = body_part
     end

spec/mail/example_emails_spec.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 
 describe "Test emails" do
-  
+
   describe "from RFC2822" do
 
     # From RFC 2822:
@@ -45,7 +45,7 @@ describe "Test emails" do
     # also that jdoe@example.org and boss@nil.test have no display names
     # associated with them at all, and jdoe@example.org uses the simpler
     # address form without the angle brackets.
-    # 
+    #
     # "Giant; \"Big\" Box" <sysservices@example.net>
     it "should handle multiple recipients test email" do
       mail = Mail.read(fixture('emails', 'rfc2822', 'example03.eml'))
@@ -77,7 +77,7 @@ describe "Test emails" do
     # conversation thread between John and Mary.  John firsts sends a
     # message to Mary, Mary then replies to John's message, and then John
     # replies to Mary's reply message.
-    # 
+    #
     # Note especially the "Message-ID:", "References:", and "In-Reply-To:"
     # fields in each message.
     it "should handle reply messages" do
@@ -213,7 +213,7 @@ describe "Test emails" do
     end
 
     # A.6.2. Obsolete dates
-    # 
+    #
     # The following message uses an obsolete date format, including a non-
     # numeric time zone and a two digit year.  Note that although the
     # day-of-week is missing, that is not specific to the obsolete syntax;
@@ -228,11 +228,11 @@ describe "Test emails" do
     end
 
     # A.6.3. Obsolete white space and comments
-    # 
+    #
     # White space and comments can appear between many more elements than
     # in the current syntax.  Also, folding lines that are made up entirely
     # of white space are legal.
-    # 
+    #
     # Note especially the second line of the "To:" field.  It starts with
     # two space characters.  (Note that "__" represent blank spaces.)
     # Therefore, it is considered part of the folding as described in
@@ -249,6 +249,13 @@ describe "Test emails" do
       doing { Mail::Message.new(email) }.should_not raise_error
     end
 
+    it "should handle folding subject" do
+      mail = Mail.read(fixture('emails', 'rfc2822', 'example14.eml'))
+      mail.from.should == ["atsushi@example.com"]
+      mail.subject.should == "Re: TEST テストテスト"
+      mail.message_id.should == '0CC5E11ED2C1D@example.com'
+      mail.body.should == "Hello"
+    end
   end
 
   describe "from the wild" do
@@ -272,18 +279,18 @@ describe "Test emails" do
       before(:each) do
         @message = Mail::Message.new(File.read(fixture('emails', 'mime_emails', 'sig_only_email.eml')))
       end
-      
+
       it "should not error on multiart/signed emails" do
         doing { @message.encoded }.should_not raise_error
       end
-      
+
       it "should have one attachment called signature.asc" do
         @message.attachments.length.should eq 1
         @message.attachments.first.filename.should eq 'signature.asc'
       end
-      
+
     end
-    
+
     describe "handling invalid group lists" do
       before(:each) do
         @message = Mail::Message.new(File.read(fixture('emails', 'error_emails', 'empty_group_lists.eml')))
@@ -299,7 +306,7 @@ describe "Test emails" do
     end
 
   end
-  
+
   describe "empty address lists" do
 
     before(:each) do
@@ -313,7 +320,7 @@ describe "Test emails" do
     it "should return an empty groups list" do
       @message.to.should eq ['user-example@aol.com', 'e-s-a-s-2200@app.ar.com']
     end
-    
+
   end
-  
+
 end

spec/fixtures/emails/rfc2822/example14.eml
@@ -1,0 +1,16 @@
+From test@example.com  Mon Aug 22 09:45:15 2011
+Date: Fri, 19 Aug 2011 10:47:17 +0900
+From: Atsushi Yoshida <atsushi@example.com>
+Reply-To: rudeboyjet@gmail.com
+Subject: Re: TEST
+ 
+	=?ISO-2022-JP?B?GyRCJUYlOSVIGyhC?=
+  =?ISO-2022-JP?B?GyRCJUYlOSVIGyhC?=
+To: rudeboyjet@gmail.com
+Message-Id: <0CC5E11ED2C1D@example.com>
+In-Reply-To: <rid_5582199198@msgid.example.com>
+Mime-Version: 1.0
+Content-Type: text/plain; charset=iso-2022-jp
+Content-Transfer-Encoding: 7bit
+
+Hello

commit dcc4772093e923b6ebbe7861ff854e75d9a21d72

Split on an exact \r\n\r\n sequence if it exists
This has been broken since 17783f8536fc09b926c7425dbacfc35e0e851ef5

lib/mail/message.rb
@@ -1889,7 +1889,7 @@ module Mail
     # Additionally, I allow for the case where someone might have put whitespace
     # on the "gap line"
     def parse_message
-      header_part, body_part = raw_source.lstrip.split(/#{CRLF}#{WSP}*#{CRLF}(?!#{WSP})/m, 2)
+      header_part, body_part = raw_source.lstrip.split(/#{CRLF}#{CRLF}|#{CRLF}#{WSP}*#{CRLF}(?!#{WSP})/m, 2)
       self.header = header_part
       self.body   = body_part
     end

spec/fixtures/emails/plain_emails/raw_email_starting_with_space.eml
@@ -1,0 +1,27 @@
+Received: from tools02.corp.linkedin.com (172.18.46.250) by
+ esv4-cas02.linkedin.biz (172.18.46.142) with Microsoft SMTP Server id
+ 14.1.355.2; Fri, 25 Jan 2013 14:32:09 -0800
+Content-Type: text/plain; charset="us-ascii"
+Content-Transfer-Encoding: 7bit
+From: CRT Notifications <crt-notifications-noreply@linkedin.com>
+To: Example <example@example.com>
+Subject: Reverting back 46964b45b3a2d50c8c8777128a5d8f179a121b77,
+	Please refer to
+	https://mp-trunkdev-hudson.corp.linkedin.com/job/MP_TRUNKDEV_POSTCOMMIT/969/
+Message-ID: <82077049-8e7b-4f95-90e8-84da7b2f9514@ESV4-CAS02.linkedin.biz>
+Return-Path: crt-notifications-noreply@linkedin.com
+Date: Fri, 25 Jan 2013 14:32:09 -0800
+X-MS-Exchange-Organization-AuthSource: esv4-cas02.linkedin.biz
+X-MS-Exchange-Organization-AuthAs: Internal
+X-MS-Exchange-Organization-AuthMechanism: 0a
+X-Originating-IP: [172.18.46.250]
+MIME-Version: 1.0
+
+      Hello example,
+
+      Reverted back 46964b45b3a2d50c8c8777128a5d8f179a121b77, Please refer to https://mp-trunkdev-hudson.corp.linkedin.com/job/MP_TRUNKDEV_POSTCOMMIT/969/
+      The replacement revision is 127ca8c104fd96ca35edeb09dd4c706d792cb2fc.
+
+      Please make sure that the issue is fixed when you recommit your changes.
+
+      Thanks.

spec/mail/example_emails_spec.rb
@@ -275,6 +275,17 @@ describe "Test emails" do
 
     end
 
+    describe "raw_email_starting_with_space.eml" do
+
+      before(:each) do
+        @message = Mail::Message.new(File.read(fixture('emails', 'plain_emails', 'raw_email_starting_with_space.eml')))
+      end
+
+      it "should parse the body" do
+        @message.body.should =~ /Reverted/
+      end
+    end
+
     describe "sig_only_email.eml" do
       before(:each) do
         @message = Mail.read(fixture('emails', 'mime_emails', 'sig_only_email.eml'))